6d73f26b3b9b270909434078078e80e13b7addc4
Denilson <denilsonsousa7@outlook.com>
2020-02-23 16:18:21 -0300

    logica de pontuação começada.

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 
 const draws = "ABCDEFABCDEF";
 const allDraws = draws.split('');
+const carts = [];
 const colection = getCarts();
+const cartsChoice = [];
 
 
-
-function verifyChoice(c){
-    for(var i = 0; i < allDraws.length; i++){
-        if(c == allDraws[i]){
-            allDraws.splice(i,1);
+function verifyChoice(c) {
+    for (var i = 0; i < allDraws.length; i++) {
+        if (c == allDraws[i]) {
+            allDraws.splice(i, 1);
             return;
         }
     }
 }
-function getCarts(){
+function getCarts() {
     const cart = document.getElementsByClassName("cart");
-  
-   
-    const carts = [];
-    
-    for(var i = 0; i < cart.length; i++){
+
+    for (var i = 0; i < cart.length; i++) {
         var ramdomNum = Math.round(Math.random() * (11 - i));
-        console.log(allDraws)
-        console.log(ramdomNum)
         var myDraw = '';
-        if(i == 12){
+        if (i == 12) {
             myDraw = allDraws[0];
-        }else{
+        } else {
             myDraw = allDraws[ramdomNum];
         }
-        
+
         verifyChoice(myDraw);
 
         carts[i] = {
@@ -40,74 +36,109 @@ function getCarts(){
             draw: myDraw
         }
 
-        
+
     }
-    for(var i = 0; i < cart.length; i++){
-        console.log(carts[i].draw);
+    for (var i = 0; i < cart.length; i++) {
+        console.log("a carta: " + (carts[i].id + 1) + " tem o drawn: " + carts[i].draw);
+
     }
 
 
     return carts;
 
 }
+function verifyExist(cart) {
+    if (cartsChoice.indexOf(cart) != -1) {
+        return true;
+    } else {
+        return false;
+    }
 
-function setValues(){
+}
+function clearArray(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        cartsChoice.pop();
+    }
+}
+function verifyStatus() {
+    if (cartsChoice.length > 0) {
+        var firstCartChoice = cartsChoice[0];
+    }
+    for (var i = 0; i < carts.length; i++) {
+        if (carts[i].status && !verifyExist(carts[i])) {
+            cartsChoice.push(carts[i]);
+        }
+    }
+}
+function clickCart(id) {
+    if (id === '1') {
+        transformCart(id - 1);
+    }
+    if (id === '2') {
+        transformCart(id - 1);
+    }
+    if (id === '3') {
+        transformCart(id - 1);
+    }
+    if (id === '4') {
+        transformCart(id - 1);
+    }
+    if (id === '5') {
+        transformCart(id - 1);
+    }
+    if (id === '6') {
+        transformCart(id - 1);
+    }
+    if (id === '7') {
+        transformCart(id - 1);
+    }
+    if (id === '8') {
+        transformCart(id - 1);
+    }
+    if (id === '9') {
+        transformCart(id - 1);
+    }
+    if (id === '10') {
+        transformCart(id - 1);
+    }
+    if (id === '11') {
+        transformCart(id - 1);
+    }
+    if (id === '12') {
+        transformCart(id - 1);
+    }
+    verifyStatus();
+    verifyPoint();
+}
+function verifyPoint() {
+    if (cartsChoice.length == 2) {
+        console.log("Entrou");
+        var cart1 = cartsChoice[0];
+        var cart2 = cartsChoice[1];
+        console.log(cartsChoice);
+
+        if (cart1.draw == cart2.draw) {
+            console.log("point");
+            cartsChoice.splice(0,cartsChoice.length);
+            
+        }
+    }
+
+}
+function transformCart(n) {
+    colection[n].id = n + 1;
+    colection[n].status = true;
+    colection[n].cart.style.backgroundColor = "#b5c4c9";
+    colection[n].cart.style.boxShadow = "0px 10px 20px #00000066";
+    colection[n].childs[1].textContent = colection[n].draw;
+    colection[n].childs[1].style.color = "#000000aa";
+    colection[n].cart.style.color = "#000000aa";
 
 }
 
-function clickCart(id){
-    
-    if(id === '1'){
-        transformCart(id-1);
-    }
-    if(id === '2'){
-        transformCart(id-1);
-    }
-    if(id === '3'){
-        transformCart(id-1);
-    }
-    if(id === '4'){
-        transformCart(id-1);
-    }
-    if(id === '5'){
-        transformCart(id-1);
-    }
-    if(id === '6'){
-        transformCart(id-1);
-    }
-    if(id === '7'){
-        transformCart(id-1);
-    }
-    if(id === '8'){
-        transformCart(id-1);
-    }
-    if(id === '9'){
-        transformCart(id-1);
-    }
-    if(id === '10'){
-        transformCart(id-1);
-    }
-    if(id === '11'){
-        transformCart(id-1);
-    }
-    if(id === '12'){
-        transformCart(id-1);
-    }
-    
-}
-function transformCart(n){
-        colection[n].status = true;
-        colection[n].cart.style.backgroundColor = "#b5c4c9";
-        colection[n].cart.style.boxShadow = "0px 10px 20px #00000066";
-        colection[n].childs[1].textContent = colection[n].draw;
-        colection[n].childs[1].style.color = "#000000aa";
-        colection[n].cart.style.color = "#000000aa";
-
-}
-
-function changeStyle(cart){
+function changeStyle(cart) {
     cart.style.backgroundcolor = "#b5c4c9";
     const childs = cart.childs;
     console.log(childs);
-    
+
 }
