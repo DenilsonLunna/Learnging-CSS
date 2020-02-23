a02ad19da379527eb38f384c084b8aaaefadeabf
Denilson <denilsonsousa7@outlook.com>
2020-02-23 10:01:51 -0300

    Logic click ok

--- script.js
+++ script.js
@@ -1,36 +1,112 @@
-function clickCart(id){
+
+const draws = "ABCDEFABCDEF";
+const allDraws = draws.split('');
+const colection = getCarts();
+
+
+
+function verifyChoice(c){
+    for(var i = 0; i < allDraws.length; i++){
+        if(c == allDraws[i]){
+            allDraws.splice(i,1);
+            return;
+        }
+    }
+}
+function getCarts(){
     const cart = document.getElementsByClassName("cart");
+  
+   
+    const carts = [];
+    
+    for(var i = 0; i < cart.length; i++){
+        var ramdomNum = Math.round(Math.random() * (11 - i));
+        console.log(allDraws)
+        console.log(ramdomNum)
+        var myDraw = '';
+        if(i == 12){
+            myDraw = allDraws[0];
+        }else{
+            myDraw = allDraws[ramdomNum];
+        }
+        
+        verifyChoice(myDraw);
+
+        carts[i] = {
+            id: i,
+            status: false,
+            cart: cart[i],
+            childs: cart[i].childNodes,
+            draw: myDraw
+        }
+
+        
+    }
+    for(var i = 0; i < cart.length; i++){
+        console.log(carts[i].draw);
+    }
+
+
+    return carts;
+
+}
+
+function setValues(){
+
+}
+
+function clickCart(id){
+    
     if(id === '1'){
-        changeStyle(cart[0]);
+        transformCart(id-1);
     }
     if(id === '2'){
-        changeStyle(cart[1]);
+        transformCart(id-1);
     }
     if(id === '3'){
-        changeStyle(cart[2]);
+        transformCart(id-1);
     }
     if(id === '4'){
-        changeStyle(cart[3]);
+        transformCart(id-1);
     }
     if(id === '5'){
-        changeStyle(cart[4]);
+        transformCart(id-1);
     }
     if(id === '6'){
-        changeStyle(cart[5]);
+        transformCart(id-1);
     }
     if(id === '7'){
-        changeStyle(cart[6]);
+        transformCart(id-1);
     }
     if(id === '8'){
-        changeStyle(cart[7]);
+        transformCart(id-1);
     }
     if(id === '9'){
-        changeStyle(cart[8]);
+        transformCart(id-1);
     }
+    if(id === '10'){
+        transformCart(id-1);
+    }
+    if(id === '11'){
+        transformCart(id-1);
+    }
+    if(id === '12'){
+        transformCart(id-1);
+    }
+    
+}
+function transformCart(n){
+        colection[n].status = true;
+        colection[n].cart.style.backgroundColor = "#b5c4c9";
+        colection[n].cart.style.boxShadow = "0px 10px 20px #00000066";
+        colection[n].childs[1].textContent = colection[n].draw;
+        colection[n].childs[1].style.color = "#000000aa";
+        colection[n].cart.style.color = "#000000aa";
+
 }
 
 function changeStyle(cart){
-    cart.style.backgroundColor = "#b5c4c9";
+    cart.style.backgroundcolor = "#b5c4c9";
     const childs = cart.childs;
     console.log(childs);
     
